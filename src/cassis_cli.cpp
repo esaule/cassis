86e1de193a0c0e52cfb198c70fd4f65f5ea46d46
Erik Saule <esaule@bmi.osu.edu>
2012-06-25 23:10:34 -0400

    untested crude cassis_cli interface. A bug appears. after initialization, winner is player 2

--- src/cassis_cli.cpp
+++ src/cassis_cli.cpp
@@ -1,7 +1,37 @@
 #include "GameState.hpp"
+#include <iostream>
+#include <assert.h>
 
 int main()
 {
+  Cassis::Engine::GameState gs;
+  
+  while (gs.winner() != Cassis::Engine::UNCOLORED)
+    {
+      Cassis::Engine::Color pl = gs.whoseTurn();
+      assert (pl != Cassis::Engine::UNCOLORED);
+      if (pl == Cassis::Engine::PLAYER1)
+	std::cout<<"Player 1's turn"<<std::endl;
+      else 
+	std::cout<<"Player 2's turn"<<std::endl;
+
+      Cassis::Engine::Vertex i, j;
+
+      try
+	{
+	  std::cin>>i>>j;
+	  gs.play(i,j,pl);
+	}
+      catch (Cassis::Engine::InvalidParameter)
+	{
+	  std::cout<<"invalid move"<<std::endl;
+	}
+    }
+
+  if (gs.winner() == Cassis::Engine::PLAYER1)
+    std::cout<<"Player 1 wins"<<std::endl;
+  else
+    std::cout<<"Player 2 wins"<<std::endl;
 
   return 0;
 }
