19a6bad75f9d5d488988273d3931a63c62528f35
Erik Saule <esaule@bmi.osu.edu>
2012-06-25 22:42:02 -0400

    implementation of GameState

---
+++ src/GameState.cpp
@@ -0,0 +1,116 @@
+#include "GameState.hpp"
+
+Vertex GameState::nbVertex() const
+{
+  return 6;
+}
+
+Edge GameState::nbEdge() const
+{
+  return 20;
+
+  // int ret=0;
+  // for (int i=1; i <= nbVertex(); ++i)
+  //   {
+  //     ret += i;
+  //   }
+  // return ret;
+}
+
+GameState::GameState()
+  :board(new Color[nbEdge()]), turn(0)
+{
+  for (Edge e = 0; e< nbEdge(); ++e)
+    {
+      board[e] = UNCOLORED;
+    }
+}
+
+GameState::~GameState()
+{
+  delete[] board;
+  board = NULL;
+}
+
+//returns the offset of neighbors of i in a board. so \sum_{j=0}^i 6-j
+static Edge base(Vertex i)
+{
+  switch (i)
+    {
+    case 0:
+      return 0;
+    case 1:
+      return 6;
+    case 2:
+      return 11;
+    case 3:
+      return 15;
+    case 4:
+      return 18;
+    case 5:
+      return 20;
+    }
+}
+
+Color& GameState::edgeLoc(Vertex i, Vertex j)
+{
+  return board[base(i)+(j-i-1)];
+}
+
+const Color& GameState::edgeLoc(Vertex i, Vertex j) const
+{
+  return board[base(i)+(j-i-1)];
+}
+
+Color GameState::edge(Vertex i, Vertex j) const throw InvalidParameter
+{
+  if (i == j)
+    throw InvalidParameter();
+  validateVertex(i);
+  validateVertex(j);
+  if (j < i)
+    return edge(j, i);
+  
+  //assert (i < j);
+  
+  return edgeLoc(i,j);
+}
+
+Color GameState::whoseTurn() const
+{
+  switch (turn % 2)
+    {
+    case 0: return PLAYER1;
+    case 1: return PLAYER2;
+    }
+}
+
+Color GameState::winner() const
+{
+  for (Vertex x = 0 ; x < nbVertex(); x++)
+    for (Vertex y = x+1 ; y < nbVertex(); y++)
+      for (Vertex z = y+1 ; z < nbVertex(); z++)
+	{
+	  if (edge(x,y) != UNCOLORED
+	      && edge(x,y) == edge(x,z)
+	      && edge(x,y) == edge(y,z))
+	    return edge(x,y);
+	}
+  return UNCOLORED;
+}
+
+void GameState::play (Vertex i, Vertex j, Color c) throw InvalidParameter
+{
+  if (i == j)
+    throw InvalidParameter();
+  validateVertex(i);
+  validateVertex(j);
+  if (j < i)
+    play (j,i,c);
+
+  //assert (i<j);
+  if (edgeLoc(i,j) != UNCOLORED) throw InvalidParameter();
+  if (whoseTurn() != c) throw InvalidParameter();
+
+  edgeLoc(i,j) = c;
+}
--- src/GameState.hpp
+++ src/GameState.hpp
@@ -21,12 +21,36 @@ namespace Cassis
       };
 
     typedef int Vertex;
+    typedef int Edge;
 
     class GameState
     {
+      Color* board;
+      int turn;
+
+    private:
+      ///@brief throws exception if i is invalid
+      ///@param i a valid vertex. 0 \leq i < nbVertex()
+      void validateVertex(Vertex i) const throw InvalidParameter;
+
+      ///@brief returns a reference on a given edge. Note, the constraint i < j MUST be enforced prior to calling this function. (Or pay a segfault)
+      ///@param i a valid vertex. 0 \leq i < nbVertex()
+      ///@param j an other valid vertex. 0 \leq j < nbVertex(). i \neq j
+      ///@return a reference to the color of edge (i,j)
+      const Color& edgeLoc(Vertex i, Vertex j) const;
+
+      ///@brief returns a reference on a given edge. Note, the constraint i < j MUST be enforced prior to calling this function. (Or pay a segfault)
+      ///@param i a valid vertex. 0 \leq i < nbVertex()
+      ///@param j an other valid vertex. 0 \leq j < nbVertex(). i \neq j
+      ///@return a reference to the color of edge (i,j)
+
+      Color& edgeLoc(Vertex i, Vertex j);
     public:
-      ///returns the total number of vertex in the game. By default, return 6.
+      ///returns the total number of vertices in the game. By default, return 6.
       Vertex nbVertex() const;
+
+      ///returns the total number of edges in the game.
+      Edge nbEdge() const;
 
       ///@brief returns the color of edge (i,j)
       ///@param i a valid vertex. 0 \leq i < nbVertex()
@@ -48,6 +72,9 @@ namespace Cassis
       ///@param j an other valid vertex. 0 \leq j < nbVertex(). i \neq j
       ///@param c either PLAYER1 or PLAYER2
       void play(Vertex i, Vertex j, Color c) throw InvalidParameter;
+
+      GameState();
+      ~GameState();
     };
   }
 }
